45fdc398090c70033a9b61c14e5afdede51b0b96
Ken.Song <kensong96@gmail.com>
2019-08-23 18:43:22 -0400

    question model made

---
+++ Quizzler/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Zhen Song on 2019-08-23.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+class Question{
+    let questionText : String
+    let answer : Bool
+    
+    init(text : String, correctAnswer : Bool){
+        questionText = text
+        answer = correctAnswer
+    }
+}
